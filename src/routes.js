8996dbf80c9b81d123c91662d273b4512cd5e425
Alberto Forni <alberto.forn@gmail.com>
2016-03-23 21:32:04 +0000

    fix index route component

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App/App';
 
+function SimpleDiv() {
+  return <p>hello world</p>;
+};
+
 export default (
-	<Route path='/' component={App}>
-		<IndexRoute component={<div />} />
-	</Route>
+  <Route path='/' component={App}>
+    <IndexRoute component={SimpleDiv} />
+  </Route>
 );
